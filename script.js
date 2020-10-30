7858f8f654db9e9410428b8fdf4a0ef930641e9b
David Harris <jdh.programmer@gmail.com>
2020-10-29 20:47:56 -0500

    Code done, working perfectly, added a Copy Password button

--- script.js
+++ script.js
@@ -1,62 +1,95 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+    let password = generatePassword();
+    let passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
 
 }
+
+// Define Copy password button & add Event listener
+let copyBtn = document.querySelector("#copy")
+copyBtn.addEventListener("click", copyPassword);
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+// Define password character option Strings & Arrays
 let lowercaseArray = "abcdefghijklmnopqrstuvwxyz";
 let uppercaseArray = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let specialCharArray = "!#$%&'()*+,-./:;<=>?@[^_`{|}~";
-let numberArray = [0,1,2,3,4,5,6,7,8,9];
+let numberArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let passwordArray = [];
 
+// Main Function to Prompt the user for Password Choices and Generate the Random Password
+function generatePassword() {
 
-
-function generatePassword(pwLength, yesLowercase, yesUppercase, yesNumbers, yesSpecialChar) {
-
-  let pwLength = prompt("How many characters would you like your password to contain?")
-  
-  if (pwLength < 8 || pwLength > 128 || isNaN(pwLength)) {
-    alert("I know, I know... here come the rules...\nThere's only one rule:\n1. You Do Not talk about Fight Club!\nokay... there's 2 rules.\n2. Password length must be between 8 - 128.")
     let pwLength = prompt("How many characters would you like your password to contain?")
-  }
-  // let superArray = [""]; 
-  // let chosenChars = haveItYourWay()
 
-  let yesLowercase = confirm("Would you like to include Lowercase Letters?");
-  let yesUppercase = confirm("Would you like to include Uppercase Letters?");
-  let yesNumbers = confirm("Would you like to include Numbers?");
-  let yesSpecialChar = confirm("Would you like to include Special Characters?");
-  
-  if (yesLowercase === false && yesUppercase === false && yesNumbers === false && yesSpecialChar === false) {
-    alert("Please select at least one option\n[3+ options RECOMMENDED]")
-    haveItYourWay()
-  }
-  if (yesLowercase) {
-    return lowercaseArray,
-  }
-  if (yesUppercase) {
-    return uppercaseArray,
-  }
-      
+    if (pwLength < 8 || pwLength > 128 || isNaN(pwLength)) {
+        alert("I know, I know... here come the rules...\nThere's only one rule:\n1. You Do Not talk about Fight Club!\nokay... there's 2 rules.\n2. Password length must be between 8 - 128.")
+        prompt("How many characters would you like your password to contain?")
+    }
+    // Character Choice Confirms & Minimum 1 choice Validation Funcion
+    haveItYourWay();
+
+    function haveItYourWay() {
+
+        let yesLowercase = confirm("Would you like to include Lowercase Letters?");
+        let yesUppercase = confirm("Would you like to include Uppercase Letters?");
+        let yesNumbers = confirm("Would you like to include Numbers?");
+        let yesSpecialChar = confirm("Would you like to include Special Characters?");
+
+        if (yesLowercase) {
+            passwordArray = [...passwordArray, ...lowercaseArray];
+        }
+        if (yesUppercase) {
+            passwordArray = [...passwordArray, ...uppercaseArray];
+        }
+        if (yesNumbers) {
+            passwordArray = [...passwordArray, ...numberArray];
+        }
+        if (yesSpecialChar) {
+            passwordArray = [...passwordArray, ...specialCharArray];
+        }
+
+        if (yesLowercase === false && yesUppercase === false && yesNumbers === false && yesSpecialChar === false) {
+            alert("Please select at least one option\n[3+ options RECOMMENDED]")
+            haveItYourWay();
+        }
+    }
+    // Define String for Random Password & Generate random characters According to desired Password Length
+    let passwordCharacters = "";
+    for (let i = 0; i < passwordArray.length; i++) {
+        let characters = passwordArray[Math.floor(Math.random() * passwordArray.length)];
+        passwordCharacters = passwordCharacters + characters;
+    }
+    document.querySelector("#copy")
+    copyBtn.setAttribute("style", "visibility: visible")
+
+
+    return passwordCharacters;
 }
-    
 
+function copyPassword() {
 
-
-function adderReturn(x, y, z) {
-  return x + y + z;
+    let copyText = document.querySelector("#password");
+    copyText.select();
+    document.execCommand("copy");
+    alert("Copied the text: " + copyText.value);
 }
-var result = adderReturn(1, 2, 3);
-console.log(result);
+
+
+
+// function adderReturn(x, y, z) {
+//   return x + y + z;
+// }
+// let result = adderReturn(1, 2, 3);
+// console.log(result);
 
 
 // GIVEN I need a new, secure password
